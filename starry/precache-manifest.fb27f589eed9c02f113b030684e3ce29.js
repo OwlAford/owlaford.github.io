9cd071495a441d2b547ecabcbfea244fda6c5e74
OwlAford <244734834@qq.com>
2018-10-23 10:43:25 +0800

    update

---
+++ starry/precache-manifest.fb27f589eed9c02f113b030684e3ce29.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = [
+  {
+    "revision": "e71609d1b1c66a108c0fb066eba5a995",
+    "url": "/starry/img/avatar.e71609d1.jpg"
+  },
+  {
+    "revision": "35cf1f5e115011c7f331873143a20504",
+    "url": "/starry/mock/login.json"
+  },
+  {
+    "revision": "6762a2092660706da5a49e23c714b4ab",
+    "url": "/starry/img/fail-icon.6762a209.svg"
+  },
+  {
+    "revision": "0f0f9f1f17953974ebfa",
+    "url": "/starry/js/asset.05c35513.js"
+  },
+  {
+    "revision": "45a5ec9752e68382711c",
+    "url": "/starry/js/channel.81570425.js"
+  },
+  {
+    "revision": "bd6c7b91880cf0ea806401c28cb46db4",
+    "url": "/starry/mock/address.json"
+  },
+  {
+    "revision": "ca5ba027f02040f26415",
+    "url": "/starry/js/chunk-vendors.48fb2b53.js"
+  },
+  {
+    "revision": "1b337da7a5b809d17ca1",
+    "url": "/starry/js/gyro.952b46c2.js"
+  },
+  {
+    "revision": "ff92e83f444d3df64902",
+    "url": "/starry/js/home.0d56e35b.js"
+  },
+  {
+    "revision": "744dab002fe325ebd2c7",
+    "url": "/starry/js/map.20c018ea.js"
+  },
+  {
+    "revision": "623a89fec3012e152668",
+    "url": "/starry/js/pixel.4965a30d.js"
+  },
+  {
+    "revision": "12031b1f3ca4f023df20",
+    "url": "/starry/js/scroll.cd5f6962.js"
+  },
+  {
+    "revision": "a733f5a27c22288f8da2",
+    "url": "/starry/js/sector.66ccd464.js"
+  },
+  {
+    "revision": "d0fab84213a848951c39",
+    "url": "/starry/js/star.760e7f62.js"
+  },
+  {
+    "revision": "2ee22069250676dee4cf",
+    "url": "/starry/js/wave.81370eff.js"
+  },
+  {
+    "revision": "38c2a8ecc01bcf943ed6f142cd329242",
+    "url": "/starry/mock/home.json"
+  },
+  {
+    "revision": "2fc2e53f1858187524dbf264e06d23e2",
+    "url": "/starry/mock/images.json"
+  },
+  {
+    "revision": "ca742187276c60d7f96a15556f8be1bd",
+    "url": "/starry/mock/channel.json"
+  },
+  {
+    "revision": "84245db614d35e5d1d37c20591cfa9eb",
+    "url": "/starry/mock/assets.json"
+  },
+  {
+    "revision": "a574ed5480cb33303911",
+    "url": "/starry/js/center.002c6983.js"
+  },
+  {
+    "revision": "9ac5bcce18e0d543654c97a8e2c192a1",
+    "url": "/starry/img/noresult.9ac5bcce.svg"
+  },
+  {
+    "revision": "f8ba71315e305f8c2bf0b6f354d9ce1d",
+    "url": "/starry/img/yeah.f8ba7131.png"
+  },
+  {
+    "revision": "e1a42ff1de6b1ae26634",
+    "url": "/starry/js/app.c1f0a89e.js"
+  },
+  {
+    "revision": "e46d160e4d44fc120598bde3e24e8ed8",
+    "url": "/starry/index.html"
+  },
+  {
+    "revision": "7ffd1f4ebac736bf21c499dc7156e76b",
+    "url": "/starry/img/silly.7ffd1f4e.png"
+  },
+  {
+    "revision": "de71b67a45788da0349cca9620ff1987",
+    "url": "/starry/img/logo.de71b67a.png"
+  },
+  {
+    "revision": "e2ae4d110b8753718ef36c836cb6d88d",
+    "url": "/starry/img/round.e2ae4d11.svg"
+  },
+  {
+    "revision": "d566a74ff897fe6de2bece5178bab388",
+    "url": "/starry/img/triangle.d566a74f.svg"
+  },
+  {
+    "revision": "eaadb1164e69c49576c5b11865798395",
+    "url": "/starry/img/kill.eaadb116.png"
+  },
+  {
+    "revision": "bafe641c066cf26a311b7071e010cbc9",
+    "url": "/starry/img/human.bafe641c.jpg"
+  },
+  {
+    "revision": "e6ec7248d47012a459873c2b439af5b5",
+    "url": "/starry/img/cnm-head.e6ec7248.svg"
+  },
+  {
+    "revision": "f3fdfa38f99b5bc4827957e73e0c4ad8",
+    "url": "/starry/img/bg.f3fdfa38.jpg"
+  },
+  {
+    "revision": "260608e53d785511d20880f906c2937e",
+    "url": "/starry/img/bg.260608e5.svg"
+  },
+  {
+    "revision": "5360672b019edabf0bdb240dcb0939c6",
+    "url": "/starry/fonts/iconfont.5360672b.ttf"
+  },
+  {
+    "revision": "ca5ba027f02040f26415",
+    "url": "/starry/css/chunk-vendors.21a5d729.css"
+  },
+  {
+    "revision": "e1a42ff1de6b1ae26634",
+    "url": "/starry/css/app.de5349f4.css"
+  }
+];
